bb618cc2dd253895e5425fae3c2dd78911283344
tolku <91892836+tolku@users.noreply.github.com>
2022-06-07 22:35:38 +0200

    tree insertion to be fixed

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import com.company.Module;
 
 public class Node {
@@ -17,42 +18,53 @@ public class Node {
         this.number = number;
     }
 
-    public void buildTree(Module currentPath, Node currentNode, List<Module> numberedModules, Bot jacek){
+    public void buildTree(Module currentPath, Node currentNode, List<Module> numberedModules, Bot jacek) {
         Node root = new Node(jacek.getCoordX(), jacek.getCoordY(), 0);
-        root.findNext(this.number, this.x, this.y, root, null);
+        root.findNext(this.number, root, null);
     }
 
-    private void findNext(int number, int coordX, int coordY, Node currentRoot, Node currentParentRoot){
+    private void findNext(int number, Node currentRoot, Node currentParentRoot) {
         //warunek stopu dla rekurencji do dodania
         Node child = null;
-        for (int counter = 0; counter < Module.getModuleList().size(); ++counter){
-            if (Module.getModuleList().get(counter).getNumber() == number + 1){
+        for (int counter = 0; counter < Module.getModuleList().size(); ++counter) {
+            if (Module.getModuleList().get(counter).getNumber() == number + 1) {
                 //sprawdzamy czy modul jest przejezdny
-                if (Module.getModuleList().get(counter).getType() == 'O'){
+                if (Module.getModuleList().get(counter).getType() == 'O') {
                     continue;
                 }
                 //musimy dodac, aby przypisaywac nastepne dzieci do dobrych rodzicow, czyli szukamy, jakie gridy z numerem number + 1, znajduja się w odleglosci 1 od obecnego gridu
-                if (currentParentRoot != null){ //tzn, ze nie jest to pierwsze przejscie i jest wiecej niz jeden lisc
+                if (currentParentRoot != null) { //tzn, ze nie jest to pierwsze przejscie i jest wiecej niz jeden lisc
                     //dodajemy liscie do naszego węzła
-                    for (int innerCounter = 0; innerCounter < currentParentRoot.children.size(); ++innerCounter){
-                        for (int innerInner = 0; innerInner < Module.getModuleList().size(); ++innerInner){
-                            if (Module.getModuleList().get(innerInner).getCoordX() == currentParentRoot.children.get(innerCounter).x + 1 || Module.getModuleList().get(innerInner).getCoordX() == currentParentRoot.children.get(innerCounter).x - 1 || Module.getModuleList().get(innerInner).getCoordY() == currentParentRoot.children.get(innerCounter).y + 1 || Module.getModuleList().get(innerInner).getCoordY() == currentParentRoot.children.get(innerCounter).y - 1 && Module.getModuleList().get(innerInner).getType() != 'O'){
+                    for (int innerCounter = 0; innerCounter < currentParentRoot.children.size(); ++innerCounter) {
+                        //tu będzie blad, musimy sprawdzic, czy dla naszego węzła istnieją następne węzly
+                        for (int innerInner = 0; innerInner < Module.getModuleList().size(); ++innerInner) {
+                            if (Module.getModuleList().get(innerInner).getCoordX() == currentParentRoot.children.get(innerCounter).x + 1 || Module.getModuleList().get(innerInner).getCoordX() == currentParentRoot.children.get(innerCounter).x - 1 || Module.getModuleList().get(innerInner).getCoordY() == currentParentRoot.children.get(innerCounter).y + 1 || Module.getModuleList().get(innerInner).getCoordY() == currentParentRoot.children.get(innerCounter).y - 1 && Module.getModuleList().get(innerInner).getType() != 'O') {
                                 child = new Node(Module.getModuleList().get(innerInner).getCoordX(), Module.getModuleList().get(innerInner).getCoordY(), Module.getModuleList().get(innerInner).getNumber());
-                                addChild(child);
+                                currentParentRoot.children.get(innerCounter).addChild(child);
                             }
                         }
                     }
-                    findNext(child.number, parent.x, parent.y, child, currentRoot);
+                    if (currentParentRoot.children.isEmpty()){
+                        throw new RuntimeException("currentParentRoot is empty!");
+                    }
+                    for (int i = 0; i < currentParentRoot.children.size(); ++i){
+                        currentRoot = currentParentRoot.children.get(i);
+                        if (currentRoot.children.isEmpty()){
+                            continue;
+                        }
+                        findNext(number + 1, currentRoot.children.get(0), currentRoot);
+                    }
+                    //findNext(number + 1, currentParentRoot.children.get(0), currentRoot);
                 }
                 //dodajemy do naszego nodea to dziecko
                 child = new Node(Module.getModuleList().get(counter).getCoordX(), Module.getModuleList().get(counter).getCoordY(), Module.getModuleList().get(counter).getNumber());
-                addChild(child);
+                currentRoot.addChild(child);
 
                 //rekurencyjnie wywolujemy szukanie nastepnych dzieci dziecka
                 //findNext(child.number, child.x, child.y, child);
             }
         }
-        findNext(child.number, parent.x, parent.y, currentRoot.children.get(0), currentRoot);
+        findNext(number + 1, currentRoot.children.get(0), currentRoot);
     }
 
     public void addChild(Node childNode) {
@@ -62,14 +74,14 @@ public class Node {
         childNode.sum = childNode.parent.value + childNode.value;
     }
 
-    public void setNodeValue(Node node){
+    public void setNodeValue(Node node) {
         value = gridValue(node);
     }
 
-    private double gridValue(Node node){
-        for (int counter = 0; counter < Module.getModuleList().size(); ++counter){
-            if (node.x == Module.getModuleList().get(counter).getCoordX() && node.y == Module.getModuleList().get(counter).getCoordY()){
-                switch (Module.getModuleList().get(counter).getType()){
+    private double gridValue(Node node) {
+        for (int counter = 0; counter < Module.getModuleList().size(); ++counter) {
+            if (node.x == Module.getModuleList().get(counter).getCoordX() && node.y == Module.getModuleList().get(counter).getCoordY()) {
+                switch (Module.getModuleList().get(counter).getType()) {
                     case 'H':
                         return 0.5;
                     case 'B':
@@ -82,7 +94,7 @@ public class Node {
         throw new RuntimeException("incorrectly initialized!?");
     }
 
-    public double findProductModuleWithTheShortestTime(Node root){
-        for ()
+    public double findProductModuleWithTheShortestTime(int number, Node currentRoot, Node currentParentRoot) {
+
     }
 }
